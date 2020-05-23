15649e4dfd91b6a114d6d740a4ec57aa7d85aa82
Iogsotot <ajustusa@gmail.com>
2020-05-23 18:13:26 +0300

    fix stepCountMessage & add guessField.disabled

--- script.js
+++ script.js
@@ -12,28 +12,31 @@ let guesses = document.querySelector(".guesses");
 let min = 0;
 let max = 100;
 
+// создание случайного "загаданного числа"
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   let result = Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-  console.log("загаданное число: " + result); // проверка на правильность работы функций
+  // console.log("загаданное число: " + result); // проверка на правильность работы функций
   return result;
 }
 let randomInt = getRandomIntInclusive(min, max);
 
+// добавляет логику для кнопки новой игры
 function newGameStart() {
     clickCount = 0;
     guessSubmit.disabled = false;
+    guessField.disabled = false;
     newGame.classList.remove("show");
     lowOrHigh.classList.remove("show");
     resultParas.classList.remove("show");
-     wrongOrCorrect.classList.remove("show");
-        wrongOrCorrect.textContent = "Не угадали!";
+    wrongOrCorrect.classList.remove("show");
+    wrongOrCorrect.textContent = "Не угадали!";
     randomInt = getRandomIntInclusive(min, max);
     guesses.textContent = "Предыдущие предположения: ";
-};
+}
 
-
+// проверка загаданного пользователем числа и основная логика приложения
 function checkNumber() {
   let guessNumber = guessField.value;
   if (clickCount > 0 && clickCount < 10) {
@@ -59,25 +62,26 @@ function checkNumber() {
     wrongOrCorrect.textContent = "Ура! Правильно!";
     lowOrHigh.classList.remove("show");
     wrongOrCorrect.classList.add("show");
-    stepCountMessage.innerHTML = "Количество шагов к победе: " + clickCount;
+    stepCountMessage.innerHTML = "Количество шагов к победе: " + (clickCount + 1);
     resultParas.append(stepCountMessage);
     guessSubmit.disabled = true;
+    guessField.disabled = true;
     newGame.classList.add("show");
   }
-};
+}
 
-
+// счетчик попыток
 function clickCounter() {
   clickCount++;
   if (clickCount >10) {
     wrongOrCorrect.textContent = "Попытки закончились, вы проиграли";
     guessSubmit.disabled = true;
+    guessField.disabled = true;
     newGame.classList.add("show");
   }
-  console.log("clickCount: " + clickCount);
-};
+}
 
 guessSubmit.addEventListener("click", checkNumber);
 guessSubmit.addEventListener("click", clickCounter);
 newGame.addEventListener("click", newGameStart);
-// console.log("clickCount: " + clickCount);
+
